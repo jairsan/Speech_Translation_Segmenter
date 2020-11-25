f4344c4b86565e150237f6083a546a73284c80dc
Javier Iranzo <jairsan@upv.es>
2020-11-25 18:30:57 +0100

    Add default arguments, improve README and examples

--- README.md
+++ README.md
@@ -21,6 +21,13 @@ Please refer to the publication:
 }
 ```
 
+## Motivation
+
+In the cascade approach to Speech Translation, an ASR system first transcribes the audio, and then the transcriptions are translated by a downstream MT system. 
+
+Standard MT system are usually trained with sequences of around 100-150 tokens. Thus, if the audio is short, we can directly translate the transcription. However, when we have a long audio stream, the resulting transcription is many times longer than the maximum length seen by the MT model during training. This is why it is necessary to have a segmenter model that takes as input the stream of transcriped words, and outputs a stream of (hopefully) semantically self-contained segments, which are then translated independently by the MT model. The model presented here has been prepared to carry out segmentation in a streaming fashion.
+
+
 ## Overview
 
 Main requirements:
@@ -40,7 +47,7 @@ An example of the entire process for the Europarl-ST en-fr corpus is shown in th
 An specific file must be prepared for each combination of HISTORY_LENGTH and FUTURE_WINDOW_SIZE.
 ### Text data
 A text datafile consists in N lines, one for sample. Each line will consist in [1+HISTORY_LENGTH+1+FUTURE_WINDOW_SIZE] tokens separated by white-space. The first token is the label, 0 for no-split decision, 1 for split. Lets say that we are going to train a system with HISTORY_LENGTH=10, and FUTURE_WINDOW_SIZE=4.
-We are given the sentence "madam president are only greed euphoria and cheap money to be blamed for the whole mess" to segment. The corresponding sample for the split decision at position 1, "be", and target 0, is the following:
+We are given the sentence "madam president are only greed euphoria and cheap money to be blamed for the whole" to segment. The corresponding sample for the split decision at position 11, "be", and target 0, is the following:
 
 ```
 0 madam president are only greed euphoria and cheap money to be blamed for the whole
@@ -61,7 +68,7 @@ The acoustic features were extracted using the TLK software. Feel free to use an
 
 ## Model Training
 
-Training should be self-explanatory. For training a text model, you can do something like:
+Training should be self-explanatory. For training a text model using the default configuration, you can do something like:
 
 ```
 len=$(($HISTORY_LENGTH+1+$FUTURE_WINDOW_SIZE))
@@ -69,27 +76,11 @@ python train_text_model.py \
 --train_corpus train.ML$len.WS$FUTURE_WINDOW_SIZE.txt \
 --dev_corpus dev.ML$len.WS$FUTURE_WINDOW_SIZE.txt \
 --output_folder text_model \
---checkpoint_interval 10 \
 --vocabulary train.vocab.txt \
---epochs 40 \
---rnn_layer_size 256 \
---embedding_size 256 \
---n_classes 2 \
---batch_size 256 \
---min_split_samples_batch_ratio 0.3 \
---optimizer adam \
---lr 0.0001 \
---lr_schedule reduce_on_plateau \
---lr_reduce_patience 10 \
---dropout 0.3 \
 --model_architecture ff-text \
---feedforward_layers 2 \
---feedforward_size 128 \
 --sample_max_len $len \
 --sample_window_size $FUTURE_WINDOW_SIZE
 ```
-
-"--min_split_samples_batch_ratio" upsamples the data with a split/1 label. Otherwise, the model might degenerate to always output 0.
 
 Then, to train an audio model on top of a pre-trained text model,
 
@@ -100,25 +91,15 @@ python train_audio_model_from_pretrained_text.py \
 --train_audio_features_corpus train.ML$len.WS$FUTURE_WINDOW_SIZE.feas.txt \
 --dev_audio_features_corpus dev.ML$len.WS$FUTURE_WINDOW_SIZE.feas.txt \
 --model_path text_model/model.best.pt \
---output_folder audio_model \
---checkpoint_interval 1 \
---vocabulary train.vocab.txt \
---epochs 20 \
 --embedding_size 3 \
---n_classes 2 \
---batch_size 256 \
---min_split_samples_batch_ratio 0.3 \
---optimizer adam \
---lr 0.0001 \
---lr_schedule reduce_on_plateau \
---lr_reduce_patience 1 \
---dropout 0.3 \
---feedforward_layers 2 \
---feedforward_size 128 \
+--output_folder audio_model \
+--vocabulary train.vocab.txt \
 --sample_max_len $len \
 --sample_window_size $window \
 --model_architecture ff-audio-text-copy-feas
 ```
+
+It is required to provide --embedding_size [ACOUSTIC_FEATURES_DIM], which is 3 in our case.
 
 The "ff-audio-text-copy-feas" architecture corresponds to the "Audio w/o RNN" on the paper. You can also train "Audio w/ RNN" models using --model_architecture ff-audio-text and --audio_rnn_layer_size [int].
 
--- segmenter/arguments.py
+++ segmenter/arguments.py
@@ -4,18 +4,18 @@ def add_train_arguments(parser):
     parser.add_argument("--dev_corpus", type=str, help="Dev text file", required=True)
     parser.add_argument("--output_folder", type=str, help="Save model training here", required=True)
     parser.add_argument("--vocabulary", type=str, help="Vocabulary to be used by the network", required=True)
-    parser.add_argument("--epochs", type=int, help="Train the model this number of epochs",default=50)
-    parser.add_argument("--batch_size", type=int, help="Train batch size", default=64)
+    parser.add_argument("--epochs", type=int, help="Train the model this number of epochs",default=40)
+    parser.add_argument("--batch_size", type=int, help="Train batch size", default=256)
     parser.add_argument("--split_weight", type=float, help="Weight given to split samples "
                                                            "(Class 1). No split (Class 0) weight is 1)", default=1)
     parser.add_argument("--min_split_samples_batch_ratio", type=float, help="Upsample split samples (class 1) so that each"
-                                                                          "batch contains, on average, this ratio of split vs non-split", default=0.0)
+                                                                          "batch contains, on average, this ratio of split vs non-split", default=0.3)
     parser.add_argument("--log_every", type=int, default=100, help="Log training statistics every n updates")
     parser.add_argument("--checkpoint_interval", type=int, default=1, help="Save model every n epochs")
-    parser.add_argument("--optimizer", choices=['sgd', 'adam'], default="sgd")
-    parser.add_argument("--lr", type=float, default=0.001)
-    parser.add_argument("--lr_schedule", choices=['fixed', 'reduce_on_plateau'], default="fixed")
-    parser.add_argument("--lr_reduce_patience", type=int, default=5)
+    parser.add_argument("--optimizer", choices=['sgd', 'adam'], default="adam")
+    parser.add_argument("--lr", type=float, default=0.0001)
+    parser.add_argument("--lr_schedule", choices=['fixed', 'reduce_on_plateau'], default="reduce_on_plateau")
+    parser.add_argument("--lr_reduce_patience", type=int, default=10)
     parser.add_argument("--lr_reduce_factor", type=float, default=0.5)
     parser.add_argument("--unk_noise_prob", type=float, default=0.0, help = "Every word in a sample will have probability p to be replaced by \"<unk>\""
                                                                             "(Noise is applied every time a sample is drawn)" )
@@ -30,15 +30,15 @@ def add_audio_train_arguments(parser):
 
 def add_model_arguments(parser):
     # Model parameters
-    parser.add_argument("--rnn_layer_size", type=int, help="Hidden size of the RNN layers")
-    parser.add_argument("--audio_rnn_layer_size", type=int, help="Hidden size of the RNN layers for the audio encoder")
-    parser.add_argument("--embedding_size", type=int, help="Size of the embedding")
-    parser.add_argument("--feedforward_layers", type=int, help="Number of feedforward layers (Only for ff architecture)")
-    parser.add_argument("--feedforward_size", type=int, help="Size of feedforward layers (Only for ff architecture)")
-    parser.add_argument("--n_classes", type=int, choices=[2], required=False, help="Number of classification targets")
-    parser.add_argument("--dropout", type=float, default=0.0)
+    parser.add_argument("--rnn_layer_size", type=int, default=256, help="Hidden size of the RNN layers")
+    parser.add_argument("--audio_rnn_layer_size", type=int, default=8, help="Hidden size of the RNN layers for the audio encoder")
+    parser.add_argument("--embedding_size", type=int, default=256, help="Size of the embedding")
+    parser.add_argument("--feedforward_layers", type=int, default=2, help="Number of feedforward layers (Only for ff architecture)")
+    parser.add_argument("--feedforward_size", type=int, default=128, help="Size of feedforward layers (Only for ff architecture)")
+    parser.add_argument("--n_classes", type=int, choices=[2], default=2, help="Number of classification targets")
+    parser.add_argument("--dropout", type=float, default=0.3)
     parser.add_argument("--model_architecture", type=str, choices = ["simple-text", "ff-text", "ff-audio-text",
-                                                                     "ff-audio-text-copy-feas"], default = "simple_text",
+                                                                     "ff-audio-text-copy-feas"], default = "ff-text",
                         )
     parser.add_argument("--sample_max_len", type=int, help="Total number of tokens on each sample.")
     parser.add_argument("--sample_window_size", type=int, help="Number of tokens in the future window of each sample. "
