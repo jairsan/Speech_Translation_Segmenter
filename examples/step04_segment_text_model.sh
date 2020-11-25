8da877e0ccb9731e9a1fe188385ef86de9813ee4
Javier Iranzo <jairsan@upv.es>
2020-11-25 13:40:55 +0100

    Fix examples

--- examples/step04_segment_text_model.sh
+++ examples/step04_segment_text_model.sh
@@ -6,11 +6,16 @@ do
         echo test/$file >> test.lst
 done
 
+MAX_LEN=15
+FUTURE_WINDOW_SIZE=4
+
+
+
 #Now we segment
 python $PWD/../decode_text_model.py \
     --input_format list_of_text_files \
     --input_file_list $PWD/test.lst  \
     --model_path text_model/model.best.pt \
-    --sample_max_len $len \
+    --sample_max_len $MAX_LEN \
     --sample_window_size $FUTURE_WINDOW_SIZE
 
