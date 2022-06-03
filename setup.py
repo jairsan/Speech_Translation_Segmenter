f5aa8336ea449aa0c86240889546eedd9dedb77a
Javier Iranzo <jairsan@upv.es>
2022-06-03 18:35:40 +0200

    Fixes to setup

--- setup.py
+++ setup.py
@@ -1,12 +1,10 @@
-from distutils.core import setup
-
+from setuptools import setup, find_packages
 setup(
     name='Speech_Translation_Segmenter',
     version='0.1.0',
-    packages=['segmenter'],
+    packages=find_packages('segmenter'),
     url='https://github.com/jairsan/Speech_Translation_Segmenter',
     license='Apache License 2.0',
     author='Javier Iranzo-Sanchez',
-    author_email='',
     description='RNN-based streaming segmenter for Speech Translation'
 )
