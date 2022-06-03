9ad1408b9b6c0615e2e2b5f24ab6ab3b0b92aeb2
Javier Iranzo <jairsan@upv.es>
2022-06-03 18:37:53 +0200

    Fixes to setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 setup(
     name='Speech_Translation_Segmenter',
     version='0.1.0',
-    packages=find_packages('segmenter'),
+    packages=find_packages(exclude=["examples", "scripts"]),
     url='https://github.com/jairsan/Speech_Translation_Segmenter',
     license='Apache License 2.0',
     author='Javier Iranzo-Sanchez',
